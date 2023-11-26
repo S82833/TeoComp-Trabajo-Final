9aeee94927a2e312beaf92f94519e2bb78934fd9
Alice <151690667+AliceReyna@users.noreply.github.com>
2023-11-26 15:16:08 -0500

    Update run_game.py

--- run_game.py
+++ run_game.py
@@ -9,7 +9,13 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Automaton Runner')
 
 background_image = pygame.image.load("square/fondo_b.png")
-
+# Clases del juego
+# Clase para el jugador
+# Clase para el enemigo con autómata finito
+# Función para generar plataformas sin superposición vertical y con espacios saltables
+# Inicializa la fuente.
+# Función principal del juego
+# Función de game_loop()
 def run_game():
     while True:
         choice = main_menu()
