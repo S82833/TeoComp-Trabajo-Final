f92e941fc93b9e70c4905b1209c7a5591b7d8d74
Alice <151690667+AliceReyna@users.noreply.github.com>
2023-11-26 15:15:13 -0500

    Update run_game.py

--- run_game.py
+++ run_game.py
@@ -1,3 +1,15 @@
+import pygame, sys
+import random
+
+# Inicializar pygame
+pygame.init()
+
+# Configurar la ventana
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Automaton Runner')
+
+background_image = pygame.image.load("square/fondo_b.png")
+
 def run_game():
     while True:
         choice = main_menu()
@@ -6,8 +18,10 @@ def run_game():
             # Llamar a la función principal del juego
             game_loop()
         elif choice == "RULES":
+            #Llamar a la funcion de lectura a reglas
             options()
         elif choice == "QUIT":
+            #Salir del juego
             pygame.quit()
             sys.exit()
 
