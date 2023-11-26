57baee8013fda872b5f59a5dea83b1162c324484
Alice <151690667+AliceReyna@users.noreply.github.com>
2023-11-26 14:02:40 -0500

    Update game_over.py

--- game_over.py
+++ game_over.py
@@ -4,13 +4,18 @@ import random
 # Inicializar pygame
 pygame.init()
 
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Automaton Runner')
+
+background_image = pygame.image.load("square/fondo_b.png")
+
+
+FONT = pygame.font.SysFont(None, 36)  # Inicializa la fuente.
+# Función principal del juego
+
 def game_over_menu():
     while True:
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
-        
-        font = pygame.font.SysFont(None, 48)
-        text = font.render("Game Over - Press BACK to return to main menu", True, (255, 0, 0))
-        text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
         
         BACK_BUTTON = Button(image=None, pos=(400, 300), 
                               text_input="RETURN", font=get_font(50), base_color="White", hovering_color="Blue")
@@ -25,11 +30,8 @@ def game_over_menu():
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if BACK_BUTTON.checkForInput(OPTIONS_MOUSE_POS):
                     main_menu()
-            #elif event.type == pygame.KEYDOWN:
-            #    if event.key == pygame.K_BACKSPACE:
+
                     return  # Retorna al bucle principal del juego
 
-        #screen.fill(BACKGROUND_COLOR)
-        #screen.blit(text, text_rect)
-        #pygame.display.blit()
         pygame.display.update()
+
